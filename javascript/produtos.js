7ca6d1e465c532faad452afab8c8a23b0a4cb193
derick <dllduck@hotmail.com>
2021-06-14 22:18:29 -0300

    first commit :3

---
+++ javascript/produtos.js
@@ -0,0 +1,44 @@
+const produtos = [
+    {
+        id: 1,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 01',
+        description: 'Descrição do Produto 01',
+        value: 10.00
+    },
+    {
+        id: 2,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 02',
+        description: 'Descrição do Produto 02',
+        value: 15.00
+    },
+    {
+        id: 3,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 03',
+        description: 'Descrição do Produto 03',
+        value: 20.00
+    },
+    {
+        id: 4,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 04',
+        description: 'Descrição do Produto 04',
+        value: 24.50
+    },
+    {
+        id: 5,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 05',
+        description: 'Descrição do Produto 05',
+        value: 40.00
+    },
+    {
+        id: 6,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 06',
+        description: 'Descrição do Produto 06',
+        value: 55.00
+    },
+]
